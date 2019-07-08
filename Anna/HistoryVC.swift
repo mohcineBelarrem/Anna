b17267ab24856162434c8a788ab5318412189aeb
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2019-07-09 00:43:22 +0200

    Adds HistoryVC and History Scene

---
+++ Anna/HistoryVC.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryVC.swift
+//  Anna
+//
+//  Created by Mohcine Belarrem on 09/07/2019.
+//  Copyright © 2019 belarrem. All rights reserved.
+//
+
+import UIKit
+
+class HistoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
